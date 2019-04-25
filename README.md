83811b7bac2f8f8f9e7ee1c27129774b7e694e4f
Leo Zeng <qingchun1995@gmail.com>
2019-04-25 16:18:19 +0800

    implement bind hook

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ## Notes
 1. Directives accept only one argument.
+2. The benefit of modifiers is that we can use multiple at the same time.
--- app.js
+++ app.js
@@ -1,5 +1,35 @@
-Vue.directive('purple', (el) => {
-  el.style.color = 'purple'
+// Vue.directive('purple', (el) => {
+//   el.style.color = 'purple'
+// })
+
+Vue.directive('switching-color', {
+  bind (el, binding) {
+    const modifiers = binding.modifiers
+    if (modifiers.underline) {
+      el.style.textDecoration = 'underline'
+    }
+    if (modifiers.textcenter) {
+      el.style.textAlign = 'center'
+    }
+
+    const speeds = {
+      slow: 2000,
+      normal: 1000,
+      fast: 500,
+      crazy: 100
+    }
+    const speedName = binding.arg || 'normal'
+    const speed = speeds[speedName]
+
+    const colors = binding.value
+    let i = 0
+
+    setInterval(() => {
+      el.style.color = colors[i]
+
+      i = (i < colors.length - 1) ? i + 1 : 0;
+    }, speed)
+  }
 })
 
 new Vue({
@@ -7,10 +37,11 @@ new Vue({
 
   data() {
     return {
-      age: 5,
-      content: 'You are too young',
-      homepageURL: 'https://www.youtube.com/watch?v=Ok_HG-0lNCA&t=14s',
-      linkTitle: 'Vowel Sound / ɪ / as in "it"- American English Pronunciation [UPDATED] - YouTube'
+      colors: ['red', 'orange', 'blue', 'green']
     }
+  },
+
+  created() {
+    this.colors.push('pink')
   },
 })
